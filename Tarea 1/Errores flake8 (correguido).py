f81cd629957153426e6b8a9c963728fde937218d
José Rodolfo Castrillo Cubero <160172439+JoseRodolfoCastrillo@users.noreply.github.com>
2024-02-15 14:11:20 -0600

    Archivo con errores de Flake8 corregidos

---
+++ Tarea 1/Errores flake8 (correguido).py
@@ -0,0 +1,8 @@
+# Archivo con errores intencionales de flake 8
+# Tarea 1 - Microporcesadores y Microcontroladores
+import math
+
+
+def pitagoras(a, b):
+    c = math.sqrt((math.pow(a, 2) + math.pow(b, 2)))
+    return c
